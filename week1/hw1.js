7e7a9b30bca044cc8173f55ac4b80c8058d1c7e5
mdmosiurrahman <mpspmp2003@gmail.com>
2018-08-08 01:23:44 +0200

    week

---
+++ week1/hw1.js
@@ -0,0 +1,31 @@
+var  hYF06 = [    'Zeeshan Haider', 
+                  'Anas', 
+                  'Sajid',
+                  'Anuradha',
+                  'Gary', 
+                  'Marco', 
+                  'Ehsan',
+                  'Haretha',
+                  'Krishna',
+                  'Mohammad Azizul Huq',
+                  'Mohammad Mosiur Rahman',
+                  'Samara', 
+                  'Vignesh', 
+                  'Zoey Zou', 
+                  'Mohammad Subhiyeh'];
+ function randName(){
+  var array_leng = hYF06.length;
+  return Math.floor(Math.random()*array_leng)
+}
+var result1 = hYF06[randName()];
+var result2 = hYF06[randName()];
+ if(result1 == result2){
+  document.write("person1 is : " + result1);
+  document.write("<br>");
+  document.write("person2 is: " + result2);
+}
+else{
+  document.write("person1 is : " + result1);
+  document.write("<br>");
+  document.write("person2 is: " + result2);
+}
